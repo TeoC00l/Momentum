66b8e5289cf2d0093f9ed3ede0ddd3677bcdd05a
TeoC00l <37851342+TeoC00l@users.noreply.github.com>
2019-05-02 16:57:10 +0200

    Sandbox changes
    -Nothing major lol

---
+++ Plattformer01/Assets/Scripts/Interactive objects/DestructableObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class DestructableObject : MonoBehaviour
+{
+    GameObject destructableObject;
+
+    void Awake()
+    {
+        destructableObject = this.gameObject;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       Object.Destroy(destructableObject);
+    }
+}
