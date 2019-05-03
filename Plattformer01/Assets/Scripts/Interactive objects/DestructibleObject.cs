7dbe66cfe28ae01cc7bf02459394b5c12c011cc9
Meti ☆ <MetieSweetie@gmail.com>
2019-05-03 16:48:20 +0200

    debris + DestructableObject Script
    dont drag and drop debris script on anything.
    Destructible objects takes care of everything.

--- Plattformer01/Assets/Scripts/Interactive objects/DestructibleObject.cs
+++ Plattformer01/Assets/Scripts/Interactive objects/DestructibleObject.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 
 public class DestructibleObject : MonoBehaviour
@@ -10,10 +11,18 @@ public class DestructibleObject : MonoBehaviour
 
     [SerializeField] protected float destructionVelocity;
 
+    //YL attributes
+    public List<GameObject> debris = new List<GameObject>();
+    private GameObject player;
+    private GameObject itself;
+
+
     //Methods
     void Awake()
     {
         destructibleObject = gameObject.transform.parent.gameObject;
+        player = GameObject.Find("Character");
+        itself = this.gameObject;
     }
 
     void OnTriggerEnter(Collider other)
@@ -22,11 +31,28 @@ public class DestructibleObject : MonoBehaviour
 
         if (collidingPhysComp.GetVelocity().magnitude >= destructionVelocity*Time.deltaTime)
         {
-            Object.Destroy(destructibleObject);
+            spawnDebris();
+            Object.Destroy(itself); //else destroy the whole thing really
+            destructibleObject.GetComponent<Renderer>().enabled = false;
+
         }
         else
         {
             destructibleObject.layer = LayerMask.NameToLayer("Scoop");
+        }
+    }
+
+    void spawnDebris()
+    {
+        //location of object.
+        Transform spawnPos = destructibleObject.GetComponent<Transform>();
+
+        //iterate through debris spawnlist
+        for(int i = 0; i < debris.Count; i++)
+        {
+            var obj = Instantiate(debris[i], destructibleObject.GetComponent<Transform>().position,Quaternion.identity);
+            obj.AddComponent<Debris>(); //attach script to debris after it spawns.
+            //whats happening now is that you should identity it to.
         }
     }
 }
