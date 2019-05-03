0db947c80b65855c551af6c33f3c0d51c39be9df
TeoC00l <37851342+TeoC00l@users.noreply.github.com>
2019-05-03 02:19:55 +0200

    Destructible box prefab added
    ^What it says, to change the velocity at which it is destroyed, change destructionVelocity in the Child Object

---
+++ Plattformer01/Assets/Scripts/Interactive objects/DestructibleObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class DestructibleObject : MonoBehaviour
+{
+    //Attributes
+    [HideInInspector] private GameObject destructibleObject;
+    [HideInInspector] private PhysicsComponent collidingPhysComp;
+
+    [SerializeField] protected float destructionVelocity;
+
+    //Methods
+    void Awake()
+    {
+        destructibleObject = gameObject.transform.parent.gameObject;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        collidingPhysComp = other.GetComponent<PhysicsComponent>();
+
+        if (collidingPhysComp.GetVelocity().magnitude >= destructionVelocity*Time.deltaTime)
+        {
+            Object.Destroy(destructibleObject);
+        }
+        else
+        {
+            destructibleObject.layer = LayerMask.NameToLayer("Scoop");
+        }
+    }
+}
