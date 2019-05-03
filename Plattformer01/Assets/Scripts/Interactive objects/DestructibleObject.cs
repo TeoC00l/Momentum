70106c80df7777458e489510255e2322b65cd622
EOCB <evelyn.bertvall@outlook.com>
2019-05-03 17:35:17 +0200

    Revert "ddrgdgrff"
    This reverts commit 5f06c70262a7af09aa54f794e17af1e1591d7eb3, reversing
    changes made to 383732963b4a3c5407ea428fe913b3013d33231a.

--- Plattformer01/Assets/Scripts/Interactive objects/DestructibleObject.cs
+++ Plattformer01/Assets/Scripts/Interactive objects/DestructibleObject.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
 
 public class DestructibleObject : MonoBehaviour
@@ -11,18 +10,10 @@ public class DestructibleObject : MonoBehaviour
 
     [SerializeField] protected float destructionVelocity;
 
-    //YL attributes
-    public List<GameObject> debris = new List<GameObject>();
-    private GameObject player;
-    private GameObject itself;
-
-
     //Methods
     void Awake()
     {
         destructibleObject = gameObject.transform.parent.gameObject;
-        player = GameObject.Find("Character");
-        itself = this.gameObject;
     }
 
     void OnTriggerEnter(Collider other)
@@ -31,28 +22,11 @@ public class DestructibleObject : MonoBehaviour
 
         if (collidingPhysComp.GetVelocity().magnitude >= destructionVelocity*Time.deltaTime)
         {
-            spawnDebris();
-            Object.Destroy(itself); //else destroy the whole thing really
-            destructibleObject.GetComponent<Renderer>().enabled = false;
-
+            Object.Destroy(destructibleObject);
         }
         else
         {
             destructibleObject.layer = LayerMask.NameToLayer("Scoop");
-        }
-    }
-
-    void spawnDebris()
-    {
-        //location of object.
-        Transform spawnPos = destructibleObject.GetComponent<Transform>();
-
-        //iterate through debris spawnlist
-        for(int i = 0; i < debris.Count; i++)
-        {
-            var obj = Instantiate(debris[i], destructibleObject.GetComponent<Transform>().position,Quaternion.identity);
-            obj.AddComponent<Debris>(); //attach script to debris after it spawns.
-            //whats happening now is that you should identity it to.
         }
     }
 }
