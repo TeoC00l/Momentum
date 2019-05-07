51136b1f32484551f16a6dc516526bf9601330c4
EOCB <evelyn.bertvall@outlook.com>
2019-05-07 22:55:55 +0200

    Ändringar i Level 2, skapade main menu + assignade scennumber för sceneloading

---
+++ Plattformer01/Assets/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public Button playButton, quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(playListener);
+        quitButton.onClick.AddListener(quitListener);
+
+    }
+
+    void playListener()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void quitListener()
+    {
+        Application.Quit();
+    }
+
+}
