b7031d46b3301440f4d4ada8f5661132f6296f6a
EOCB <edvin.bertvall@hotmail.com>
2019-05-08 10:46:17 +0200

    Snabb victory screen för speltest

---
+++ Plattformer01/Assets/Scripts/Dummy scripts/QuitToMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuitToMenu : MonoBehaviour
+{
+    public Button quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        quitButton.onClick.AddListener(quitListener);
+    }
+
+    // Update is called once per frame
+    void quitListener()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Plattformer01/Assets/Scripts/Dummy scripts/Victory.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Victory : MonoBehaviour
+{
+    public GameObject victoryScreen;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            victoryScreen.SetActive(true);
+        }
+    }
+}
