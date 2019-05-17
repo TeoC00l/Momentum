f34407ad6db4c6cb2506c628613f4aa6d7a0b559
TeoC00l <37851342+TeoC00l@users.noreply.github.com>
2019-05-17 14:55:27 +0200

    CameraShake

--- Plattformer01/Assets/Scripts/CameraScript2.cs
+++ Plattformer01/Assets/Scripts/CameraScript2.cs
@@ -12,17 +12,64 @@ public class CameraScript2 : MonoBehaviour
     Vector3 cameraPosition = Vector3.zero;
     public Vector3 cameraOffset = Vector3.zero;
     public GameObject player;
-    
+    private bool shakeStart = false;
+    [SerializeField] private float shakeAmount;
+    [SerializeField] private LayerMask layerMask;
+    private RaycastHit hit;
+    [SerializeField] private float shakeLength;
+    private float shakeCounter = 0;
+    private bool ray = false;
+    private GameObject hitGameObject = null;
+    private float hitAgain = 0;
+    private PhysicsComponent physComp;
+
     //Methods
     void Start()
     {
         Cursor.visible = false;
         cameraOffset = transform.position - player.transform.position;
+        physComp = player.GetComponent<PhysicsComponent>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         AddRotation();
+        if (shakeStart == false)
+        {
+            Vector3 Velocity = new Vector3(physComp.GetVelocity().x, 0f, physComp.GetVelocity().z);
+            ray = Physics.Raycast(player.transform.position, Velocity.normalized, out hit, 3f, layerMask);
+            Debug.DrawRay(player.transform.position,Velocity.normalized, Color.yellow, 3f);
+
+        }
+        if (ray == true)
+        {
+            if (hit.collider.gameObject.tag == "Wall" && hit.collider.gameObject != hitGameObject )
+            {
+                hitGameObject = hit.collider.gameObject;
+                shakeStart = true;
+                
+            }
+        }else if (hitGameObject != null)
+        {
+            hitAgain += Time.deltaTime;
+        }
+        if(hitAgain > 0.1)
+        {
+
+            hitGameObject = null;
+            hitAgain = 0;
+        }
+        if (shakeStart == true)
+        {
+            ShakeCamera();
+            
+        }
+        if (shakeCounter > shakeLength)
+        {
+            shakeStart = false;
+            shakeCounter = 0;
+        }
+
     }
 
     public void AddRotation()
@@ -36,6 +83,23 @@ public class CameraScript2 : MonoBehaviour
         transform.position = player.transform.position + cameraPosition;
         player.transform.rotation = transform.rotation;
         player.transform.rotation = Quaternion.Euler(0, player.transform.eulerAngles.y, 0);
+    }
+    public void ShakeCamera()
+    {
+
+            float tempShakeAmount = physComp.GetVelocity().magnitude * shakeAmount;
+            Camera.main.transform.localPosition += new Vector3(Random.insideUnitSphere.x * tempShakeAmount, Random.insideUnitSphere.y * tempShakeAmount + 1.78f, 0f);
+            Camera.main.transform.localPosition -= new Vector3(Random.insideUnitSphere.x * tempShakeAmount, Random.insideUnitSphere.y * tempShakeAmount + 1.78f, 0f);
+            shakeCounter += Time.deltaTime;
+            
+    }
+    public void setShakeStart(bool set)
+    {
+        shakeStart = set;
+    }
+    public bool getShakeStart()
+    {
+        return shakeStart;
     }
 }
 
