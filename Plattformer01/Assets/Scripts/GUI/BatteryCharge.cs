40b0c67d1572773427fd7dac2ec5eb62a66b5ae6
Meti ☆ <MetieSweetie@gmail.com>
2019-05-21 10:54:32 +0200

    KineticBatteryUiFix.

--- Plattformer01/Assets/Scripts/GUI/BatteryCharge.cs
+++ Plattformer01/Assets/Scripts/GUI/BatteryCharge.cs
@@ -9,8 +9,10 @@ public class BatteryCharge : MonoBehaviour
     public GameObject chargeBar; //the chargebar
     public float FullChargeSize = 0.81f; //where the battery tops off
     public float minChargeSize = 0; //where the battery stops
-    public float growFactor = 50;
+    public float growFactor = 0.2f;
     private float Timer = 1000; //this is the current cooldown of kinetic battery
+    private RectTransform rectTransform;
+    private Player playerScript;
 
     //fixTimeText
 
@@ -18,25 +20,68 @@ public class BatteryCharge : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        player = GameObject.Find("Character");  //all players should be called Character
-        chargeBar = GameObject.Find("chargeBar");
-
+        player = GameObject.FindWithTag("Player");  //all players should be called Character
+        chargeBar = GameObject.Find("chargeGUI");
+        rectTransform = chargeBar.GetComponent<RectTransform>();
+        playerScript = player.GetComponent<Player>();
         //set to 0 at start 
-        chargeBar.GetComponent<RectTransform>().sizeDelta = new Vector3(0,0,0); // width and height
+        rectTransform.localScale = new Vector2(0f,0f); // width and height
     }
 
     void Update()
     {
-        if (player.GetComponent<Player>().GetKineticBatteryActive())
+        if (playerScript.GetKineticBatteryActive())
         {
+            CancelInvoke("DecreaseBar");
             //countdown
-            while (FullChargeSize > chargeBar.transform.localScale.y)
+            InvokeRepeating("IncreaseBar", 0f, 0.05f);
+            
+            if (FullChargeSize < rectTransform.localScale.x)
             {
-                Timer += Time.deltaTime;
-                chargeBar.GetComponent<RectTransform>().sizeDelta = new Vector3(minChargeSize + growFactor, minChargeSize+growFactor,0);
-            }
+                CancelInvoke("IncreaseBar");
+;           }
+               
+            
+
         }
+        else
+        {
+           
+            CancelInvoke("IncreaseBar");
+            // rectTransform.localScale = new Vector2(0f, 0f);
+            if (minChargeSize < rectTransform.localScale.x)
+            {
+                InvokeRepeating("DecreaseBar", 0f, 0.05f);
+            }
+            else
+            {
+                rectTransform.localScale = new Vector3(0f, 0.62f, 0);
+
+                CancelInvoke("DecreaseBar");
+
+            }
+            
+
+
+        }
+        
+
 
     }
+    private void IncreaseBar()
+    {
+        
+            Debug.Log("KineticActive2");
+        rectTransform.localScale = new Vector3(rectTransform.localScale.x, 0.62f, 0);
+        rectTransform.localScale += new Vector3(growFactor, 0f, 0);
+        
+    }
+    private void DecreaseBar()
+    {
 
+        Debug.Log("KineticActive2");
+        rectTransform.localScale = new Vector3(rectTransform.localScale.x, 0.62f, 0);
+        rectTransform.localScale -= new Vector3(growFactor, 0f, 0);
+
+    }
 }
