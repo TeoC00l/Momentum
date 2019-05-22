da27147f9285d68d48cf20938537bb188c7522f8
TeoC00l <37851342+TeoC00l@users.noreply.github.com>
2019-05-22 22:48:23 +0200

    WIP ya bish
    -Working on the event listeners and shiet but nothin works yet au shiet

---
+++ Plattformer01/Assets/Scripts/Abyss2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abyss2 : MonoBehaviour
+{
+    //Attributes
+    private GameObject player;
+    private Health health;
+
+    private void Start()
+    {
+        //player = GameObject.FindWithTag("Player");
+        //health = GameObject.FindWithTag("Player");
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == player)
+        {
+            //player.Health.Die();
+            //Destroy(player);
+        }
+    }
+}
